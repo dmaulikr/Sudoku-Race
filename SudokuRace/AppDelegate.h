d79d29cadcd81e5803b57dc4e4d10419f65336ab
James M. Wilsterman <JMW1@james-m-wilstermans-macbook.local>
2013-07-14 00:20:12 -0700

    Initial Commit

---
+++ SudokuRace/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SudokuRace
+//
+//  Created by James M. Wilsterman on 7/14/13.
+//  Copyright (c) 2013 James M. Wilsterman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SudokuRace/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SudokuRace
+//
+//  Created by James M. Wilsterman on 7/14/13.
+//  Copyright (c) 2013 James M. Wilsterman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
